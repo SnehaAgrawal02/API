1dcbfb46aab165b0ea9904330bbae422e9b83f93
sneha <snehaagrawal0204@gmail.com>
2024-05-15 19:20:09 +0530

    mongodblive

--- db/connectdb.js
+++ db/connectdb.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 
 
 const connectDb=()=>{
-    return mongoose.connect(process.env.LOCAL_URL)
+    return mongoose.connect(process.env.LIVE_URL)
     .then(()=>{
         console.log("connected sucessfully");
     }).catch((err)=>{
